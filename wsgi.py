cfca355294a2e126bf666180a2b4b0c6a6299c62
Victor Altamirano <60190681+victor-soporte@users.noreply.github.com>
2020-09-30 11:36:32 -0600

    Update wsgi archive
    Agrega unas lineas

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "Hola, Tepic!\r\n", 200, { 'Content-Type': 'text/plain' }
+    return "Hola, Tepic! iwuser02 \r\n", 200, { 'Content-Type': 'text/plain' }
 
 if __name__ == '__main__':
     application.run(debug = True)
